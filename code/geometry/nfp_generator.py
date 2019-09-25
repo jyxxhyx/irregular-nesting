143c71972697ca56adbb0bf99effd8f81a31747e
黄一潇 <yixiaohuang@sf-express.com>
2019-09-25 23:55:49 +0800

    Update nfp_generator.py

--- code/geometry/nfp_generator.py
+++ code/geometry/nfp_generator.py
@@ -18,7 +18,7 @@ def generate_nfp(polygon1, polygon2):
     result = pyclipper.CleanPolygons(result, 1.20)
     logger.debug('Clean nfp size: {}'.format(sum(len(element) for element in result)))
     # nfp = pyclipper.CleanPolygons(pyclipper.SimplifyPolygons(pyclipper.MinkowskiDiff(polygon1, polygon2)), 1.20)
-    return _clear_2d_list(result)
+    return _clean_empty_element_nested_list(result)
 
 
 def generate_ifp(material: Material, shape: Shape, spacing):
@@ -64,8 +64,8 @@ def diff_ifp_nfps(ifp, nfp):
     -------
 
     """
-    ifp = _clear_2d_list(ifp)
-    nfp = _clear_2d_list(nfp)
+    ifp = _clean_empty_element_nested_list(ifp)
+    nfp = _clean_empty_element_nested_list(nfp)
     pc = pyclipper.Pyclipper()
     if isinstance(ifp[0][0], numbers.Number):
         pc.AddPath(ifp, pyclipper.PT_SUBJECT, True)
@@ -96,54 +96,19 @@ def diff_ifp_nfps(ifp, nfp):
     return result
 
 
-def _clear_2d_list(_2d_list: List[List[float]]):
+def _clean_empty_element_nested_list(nested_list: List[List[float]]):
     """
     去掉CleanPolygons后出现的一些空list
     Parameters
     ----------
-    _2d_list
+    nested_list
 
     Returns
     -------
 
     """
-    _2d_list = [_1d_list for _1d_list in _2d_list if len(_1d_list) > 0]
-    return _2d_list
-
-
-def _is_ifp_nfp_rectangle_distant(ifp, nfp) -> bool:
-    """
-    判断ifp和nfp的外围矩形是否不相交。
-    Parameters
-    ----------
-    ifp
-    nfp
-
-    Returns
-    -------
-
-    """
-    if isinstance(ifp[0][0], numbers.Number):
-        ifp_min_x = min(node[0] for node in ifp)
-        ifp_min_y = min(node[1] for node in ifp)
-        ifp_max_x = max(node[0] for node in ifp)
-        ifp_max_y = max(node[1] for node in ifp)
-    else:
-        ifp_min_x = min(node[0] for each_ifp in ifp for node in each_ifp)
-        ifp_min_y = min(node[1] for each_ifp in ifp for node in each_ifp)
-        ifp_max_x = max(node[0] for each_ifp in ifp for node in each_ifp)
-        ifp_max_y = max(node[1] for each_ifp in ifp for node in each_ifp)
-    if isinstance(nfp[0][0], numbers.Number):
-        nfp_min_x = min(node[0] for node in nfp)
-        nfp_min_y = min(node[1] for node in nfp)
-        nfp_max_x = max(node[0] for node in nfp)
-        nfp_max_y = max(node[1] for node in nfp)
-    else:
-        nfp_min_x = min(node[0] for each_nfp in nfp for node in each_nfp)
-        nfp_min_y = min(node[1] for each_nfp in nfp for node in each_nfp)
-        nfp_max_x = max(node[0] for each_nfp in nfp for node in each_nfp)
-        nfp_max_y = max(node[1] for each_nfp in nfp for node in each_nfp)
-    return (nfp_max_x < ifp_min_x and nfp_max_y < ifp_min_y) or (nfp_min_x > ifp_max_x and nfp_min_y > ifp_max_y)
+    nested_list = [element for element in nested_list if len(element) > 0]
+    return nested_list
 
 
 def intersection():
